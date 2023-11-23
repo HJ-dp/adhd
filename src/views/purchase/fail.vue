d4e0c1f057b16accfdb6e6dbc140de78de88e6d9
HJ-dp <skwo777@hufs.ac.kr>
2023-11-24 00:16:46 +0900

    refactor : router

---
+++ src/views/purchase/fail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="cont">
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .cont {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
