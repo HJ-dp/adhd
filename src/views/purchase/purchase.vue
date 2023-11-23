78ed50df5ffb951be8f5268b8022095d32b08b4c
HJ-dp <skwo777@hufs.ac.kr>
2023-11-23 17:54:57 +0900

    feat : Add toss

--- src/views/purchase/purchase.vue
+++ src/views/purchase/purchase.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
-
+    <div class="hello">
+      <h1>주문서</h1>
+      <div id="payment-method"></div>
+      <button @click="clickPaymentButton">결제하기</button>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style scoped>
-
-</style>
+  </template>
+  
+  <script>
+  import { generateRandomString } from "../utils/util";
+  
+  const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
+  const paymentWidget = PaymentWidget(clientKey, PaymentWidget.ANONYMOUS);
+  
+  export default {
+    mounted() {
+      paymentWidget.renderPaymentMethods(
+        "#payment-method",
+        { value: 50000 },
+        { variantKey: "DEFAULT" }
+      );
+    },
+    methods: {
+      clickPaymentButton() {
+        paymentWidget
+          .requestPayment({
+            orderId: generateRandomString(),
+            orderName: "리팩터링 2판 외 1권",
+            successUrl: window.location.origin + "/success",
+            failUrl: window.location.origin + "/fail",
+            customerEmail: "customer123@gmail.com", // 고객 이메일 (선택)
+            customerName: "김토스", // 고객 이름 (선택)
+          })
+          .then((v) => console.log(v));
+      },
+    },
+  };
+  </script>
